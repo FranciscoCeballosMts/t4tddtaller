04528855b1af5595ff5b146f097e07761d437e63
Pancho36 <151878674+Pancho36@users.noreply.github.com>
2024-11-15 12:54:17 -0300

    Test 5 pasado

--- src/main/java/com/example/dev/Main_Kaprekar.java
+++ src/main/java/com/example/dev/Main_Kaprekar.java
@@ -15,7 +15,27 @@ public class Main_Kaprekar {
 
     }
 
-   
+    public static int ordenarMayor(){
+        int b=0;
+        return b ;
+
+    }
+
+    public static int kaprekarOp(int i) {
+        String numeroStr = Integer.toString(Math.abs(i));
+        char[] digitos = numeroStr.toCharArray();
+        Arrays.sort(digitos);
+        StringBuilder numero = new StringBuilder(new String(digitos));
+        StringBuilder numeroMayor = new StringBuilder(new String(digitos));
+        numeroMayor.reverse();
+        int menor = Integer.parseInt(numero.toString());
+        int mayor = Integer.parseInt(numeroMayor.toString());
+        return mayor-menor;
+    }
+    public static int itKaprekar(int i){
+        return 3;
+    }
+
 //aquí van sus funciones:
 
 }
