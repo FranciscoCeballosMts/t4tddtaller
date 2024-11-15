1bec5866f7c806e8baa8626bf6b4d572efdc002b
Francisco Ceballos <151878674+FranciscoCeballosMts@users.noreply.github.com>
2024-11-15 12:48:42 -0300

    Update Main_Kaprekar.java

--- src/main/java/com/example/dev/Main_Kaprekar.java
+++ src/main/java/com/example/dev/Main_Kaprekar.java
@@ -11,28 +11,11 @@ public class Main_Kaprekar {
 
 
     public static void main(String[] args) {
-        kaprekarOp(13);
+        
 
     }
 
-    public static int ordenarMayor(){
-        int b=0;
-        return b ;
-
-    }
-
-    public static int kaprekarOp(int i) {
-        String numeroStr = Integer.toString(Math.abs(i));
-        char[] digitos = numeroStr.toCharArray();
-        Arrays.sort(digitos);
-        StringBuilder numero = new StringBuilder(new String(digitos));
-        StringBuilder numeroMayor = new StringBuilder(new String(digitos));
-        numeroMayor.reverse();
-        int menor = Integer.parseInt(numero.toString());
-        int mayor = Integer.parseInt(numeroMayor.toString());
-        return mayor-menor;
-    }
-
+   
 //aquí van sus funciones:
 
 }
